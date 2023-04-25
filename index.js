eb513722476d43ec62367a4dc20e9a2b85ad5ff2
Amit Giri <thisisag840@gmail.com>
2023-04-25 23:09:55 +0530

    get, post added

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const RegNo = req.body.reg_num;
+
+    console.log(`Name: ${name}, Email: ${email}, Registration Number: ${RegNo}`);
+
+    res.send(`Name: ${name}, Email: ${email}, Registration Number: ${RegNo}`);
+});
+
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-  });
+});
