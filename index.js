98b7268dd6721a67ee4a01444384e5f9b3cdca9a
Amit Giri <thisisag840@gmail.com>
2023-04-26 10:29:28 +0530

    conditions added

--- index.js
+++ index.js
@@ -14,25 +14,38 @@ app.get('/', (req, res) => {
 app.post('/submit', (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
-  const RegNo = req.body.reg_num;
+  const Roll = req.body.Roll;
+  if (Roll >= 1 && Roll <= 100) {
 
-  console.log(`Name: ${name}, Email: ${email}, Registration Number: ${RegNo}`);
+    console.log(`Name: ${name}, Email: ${email}, Roll Number: ${Roll}`);
 
-  fs.readFile(__dirname + '/dataReceived.html', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
+    fs.readFile(__dirname + '/dataReceived.html', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
 
-    const replacedData = data
-      .replace('{{name}}', name)
-      .replace('{{email}}', email)
-      .replace('{{regNum}}', RegNo);
+      const replacedData = data
+        .replace('{{name}}', name)
+        .replace('{{email}}', email)
+        .replace('{{Roll}}', Roll);
 
-    res.send(replacedData);
-  });
+      res.send(replacedData);
+    });
+  }
+  else {
+    res.status(400).send('Roll Number is not valid.');
+  }
 });
+
+const errorhandler = (req, res) => {
+  res.status(404).send(`
+    <h1>404 Error: Page Not Found</h1>
+  `);
+};
+
+app.use(errorhandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
